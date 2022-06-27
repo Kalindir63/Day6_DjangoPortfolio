8e9cfaec56c7f7ea833cbeff70180b3bab43610a
Bradley Bailey <bradley@comprsa.com>
2022-06-27 17:06:47 +0200

    Initial commit

---
+++ Day6_DjangoPortfolio/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Day6_DjangoPortfolio project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Day6_DjangoPortfolio.settings')
+
+application = get_asgi_application()
